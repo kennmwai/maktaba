be735d03d978cf2425f8aa90649f2351f3e4982d
FourtyThree43 <shaqmwa@outlook.com>
2025-02-18 00:45:36 +0300

    Initial Commit

---
+++ src/main/java/com/ken/maktaba/MaktabaApplication.java
@@ -0,0 +1,13 @@
+package com.ken.maktaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaktabaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaktabaApplication.class, args);
+	}
+
+}
